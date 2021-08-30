fa724fc89a0b4f62257650d1265722bb0adf0c61
kaushik <gorrepatikaushik@gmail.com>
2021-08-29 20:56:58 -0500

    First Commit

---
+++ src/ClassList.java
@@ -0,0 +1,11 @@
+
+public class ClassList {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassGrades myClassList = new ClassGrades();
+		myClassList.printClassList();
+
+	}
+
+}
